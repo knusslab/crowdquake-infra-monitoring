3b2c5d30c7e473fa689c44fc2a317211777e6b8d
redbeanbread031 <redbeanbread031@gmail.com>
2025-07-09 14:11:06 +0900

    refactor: rename data-collector module to container-data-collector and localhost-data-collector to machine-data-collector

---
+++ metrics-backend/container-data-collector/src/test/java/kr/cs/interdata/containerdatacollector/DataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.cs.interdata.containerdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

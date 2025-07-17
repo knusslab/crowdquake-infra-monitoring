6873f45125bb0f8a8079548b72f11207338e32f1
redbeanbread031 <redbeanbread031@gmail.com>
2025-07-17 15:13:44 +0900

    docs: add annotation for /api-backend/config/ ... files

--- api-backend/src/main/java/kr/cs/interdata/api_backend/config/WebsocketConfig.java
+++ api-backend/src/main/java/kr/cs/interdata/api_backend/config/WebsocketConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+/**
+ * 웹소켓 관련 설정을 담당하는 클래스입니다.
+ */
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
@@ -14,7 +17,8 @@ public class WebsocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // 웹소켓 엔드포인트: /ws/metrics
+        // "/ws/metrics" 엔드포인트로 들어오는 웹소켓 연결 요청을 MetricWebsocketHandler로 처리하도록 등록
+        // setAllowedOrigins("*")는 모든 도메인에서의 웹소켓 연결을 허용 (내부 서비스 사용이라 허용해놓음)
         registry.addHandler(new MetricWebsocketHandler(), "/ws/metrics")
                 .setAllowedOrigins("*");
     }
